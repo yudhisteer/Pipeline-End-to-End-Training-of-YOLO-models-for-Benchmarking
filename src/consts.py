69a65de203708a14feb5dbc5f9180652bd804c56
yudhisteer <cyudhist@amazon.com>
2025-08-08 12:04:55 -0700

    feat: set constant for dir

---
+++ src/consts.py
@@ -0,0 +1,4 @@
+TRAIN_IMAGES_PATH = "dataset/train/data"
+TRAIN_LABELS_PATH = "dataset/train/labels.json"
+VAL_IMAGES_PATH = "dataset/validation/data"
+VAL_LABELS_PATH = "dataset/validation/labels.json"
