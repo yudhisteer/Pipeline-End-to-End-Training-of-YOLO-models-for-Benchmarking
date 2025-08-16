3fc304f9c7813c55a2e22490f0949141902e5940
yudhisteer <cyudhist@amazon.com>
2025-08-15 17:56:49 -0700

    refactor: move custom exceptions to utils dir

---
+++ src/sagemaker/utils/utils_exceptions.py
@@ -0,0 +1,9 @@
+class ModelLoadError(Exception):
+    """Exception raised when model loading fails."""
+    pass
+
+
+class InferenceError(Exception):
+    """Exception raised during inference."""
+    pass
+
